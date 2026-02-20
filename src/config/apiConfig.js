95c3e974af96ceb10aefa6757c32f47a852f0b40
humors2025 <humorstech22@gmail.com>
2026-02-20 17:45:33 +0530

    weekly_analysis_complete_lambda

--- src/config/apiConfig.js
+++ src/config/apiConfig.js
@@ -31,7 +31,8 @@ export const API_ENDPOINTS = {
   },
   MEALANALYSIS:{
  WEEKLYANALYSISCOMPLETE:"/dietitian/api/web/weekly_analysis_complete.php",
- WEEKLYANALYSISCOMPLETE1:"/dietitian/api/web/weekly_analysis_complete1.php",
+//  WEEKLYANALYSISCOMPLETE1:"/dietitian/api/web/weekly_analysis_complete1.php",
+ WEEKLYANALYSISCOMPLETE1:"/dietitian/api/web/weekly_analysis_complete_lambda.php",
  CHECKWEEKLYANALYSIS:"/dietitian/api/web/check_weekly_analysis.php",
  ADDFOOD:"/dietitian/api/web/save_weekly_food_json.php",
  GETFOOD:"/dietitian/api/web/get_save_weekly_food_json.php"
